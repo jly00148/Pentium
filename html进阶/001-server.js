947ee336e37ab4330fe64c99e5ac466814185e98
jly <453825818@qq.com>
2019-11-14 14:39:06 +0800

    cookie expiration time

--- html进阶/001-server.js
+++ html进阶/001-server.js
@@ -6,7 +6,12 @@ var url = require('url');
 var server = http.createServer(function(req,res) {
 	var urlStr = req.url;
 	console.log('Cookie:::',req.headers.cookie);
-	res.setHeader('Set-Cookie',["username=jly111"]);
+
+	// var oDate = new Date('2019-11-14 14:35:00').toUTCString();
+	// res.setHeader('Set-Cookie',["username=jly111;expires="+ oDate]);
+
+	res.setHeader('Set-Cookie',["username=jly111;max-age=10"]);	
+
 	if(urlStr == '/favicon.ico'){
 		res.end('favicon.ico');
 	}
