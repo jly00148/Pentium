3f23522333b6f6db43191a74092af3d6ac38a954
jly <453825818@qq.com>
2019-11-26 16:45:57 +0800

    flie module and global pass values

---
+++ nodeJS/01-module/file/m03.js
@@ -0,0 +1,2 @@
+const str = 'hello';
+global.str1 = str;
---
+++ nodeJS/01-module/file/m04.js
@@ -0,0 +1,5 @@
+// console.log(str);
+require('./m03.js');
+//console.log(global);
+console.log(global.str1);//hello
+console.log(str1);//hello
