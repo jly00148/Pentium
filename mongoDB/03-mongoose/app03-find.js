613be5983e15ce394629da6e28175acaa5934ec5
jly <453825818@qq.com>
2019-12-09 19:52:39 +0800

    find document

---
+++ mongoDB/03-mongoose/app03-find.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+
+//1 连接数据库
+mongoose.connect('mongodb://localhost/kuazhu',{uswNewParser:true});
+
+const db = mongoose.connection;
+db.on('error',(err)=>{
+console.log('connection error');
+throw err;
+});
+
+db.once('open',()=>{
+	console.log('connection successful');
+	//2 定义schema
+	const UserSchema = new mongoose.Schema({
+		name:String,
+		age:Number,
+		major:String
+	});
+	//3  生成模型Model
+	//3.1 mongoose.model第一个参数指定集合的名称,mongoose会自动变为复数
+	//3.2 mongoose.model第二个参数指定Schema
+	const UserModel = mongoose.model('user',UserSchema);
+
+/*
+	UserModel.find({age:{$gt:50}},(err,docs)=>{
+		if(err){
+			console.log('find err',err);
+		}else{
+			console.log(docs);
+		}
+	})
+*/
+	UserModel.find({age:{$gt:50}},'name age -_id',(err,docs)=>{
+		if(err){
+			console.log('find err',err);
+		}else{
+			console.log(docs);
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+});
