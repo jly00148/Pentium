813ee78306c69f53c375abf0c8f12987e20dfc0d
jly <453825818@qq.com>
2019-12-12 22:20:38 +0800

    clear folder

--- webpack/02-plugin/webpack.config.js
+++ webpack/02-plugin/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
 	//指定打包文件
 	mode:'development',
@@ -51,6 +52,7 @@ module.exports = {
 			filename:'index.html',//输出的文件名
 			//inject:'head',//脚本写在那个标签里,默认是true(在body结束语后)
 			hash:true //给生成的js/css文件添加一个唯一的hash
-		})
+		}),
+		new CleanWebpackPlugin()
 	]
 };
