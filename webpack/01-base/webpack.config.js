924330daebae1141aae8ebbe05fc6249ecf3321c
jly <453825818@qq.com>
2019-12-12 17:39:37 +0800

    entry and output

--- webpack/01-base/webpack.config.js
+++ webpack/01-base/webpack.config.js
@@ -4,11 +4,21 @@ module.exports = {
 	//指定打包文件
 	mode:'development',
 	//指定出口
-	entry:'./src/index.js',
+	//多个出口
+	entry:{
+		main:'./src/index.js',
+		about:'./src/about.js',
+		contact:'./src/contact.js'
+	},
+	//单个出口
+	//entry:'./src/index.js',
+
+
+
 	//指定出口
 	output:{
 		//出口文件名称
-		filename:'bundle.js',
+		filename:'[name].[hash].bundle.js',
 		//出口文件 所在的目录
 		path:path.resolve(__dirname,'dist')
 	},
