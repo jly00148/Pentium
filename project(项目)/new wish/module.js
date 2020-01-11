684cacb0e1c8a8502a1db8dbb1d1e2093cf8152f
jly <453825818@qq.com>
2020-01-11 22:18:11 +0800

    gets server data

---
+++ project(项目)/new wish/module.js
@@ -0,0 +1,142 @@
+const fs = require('fs');
+//console.log(fs.readFile); [Function: readFile]
+
+const util = require('util');
+//console.log(util); object
+
+
+let filePath = ('./data/wish.json');
+console.log(filePath);
+let readFile = util.promisify(fs.readFile);
+let writeFile = util.promisify(fs.writeFile);
+//console.log(''+fs.readFile);
+//console.log(""+readFile);
+/*
+async function add(name){
+	//1. 打开文件
+	let arr = await readFile(filePath);
+	
+	let strArr = JSON.parse(arr);
+	//console.log(data); []
+
+	//2. 写入文件
+	strArr.push({
+		id:Date.now().toString() + parseInt(Math.random() * 10000).toString().padStart(4,'0'),
+		name:name
+	})
+	//3. 保存文件
+
+	let endStr = JSON.stringify(strArr);
+	await writeFile(filePath,endStr);
+
+	return strArr;
+}
+add('Leo')
+.then(data=>{
+	console.log(data);
+},err=>{
+	console.log(err);
+})
+*/
+
+
+
+
+async function getAll(){
+	let arr = await readFile(filePath);
+	//console.log(arr);	
+
+    let strArr = JSON.parse(arr);
+    return strArr;
+	//console.log(strArr);  [ { id: '15783879724635874', name: 'jly' } ]
+
+	//return let obj = strArr.find(val=>{
+        
+    /*
+	return strArr.find(val=>{
+		//console.log(val); { id: '15783879724635874', name: 'jly' }
+		//console.log(val.id == val['id']); true
+		//console.log(val.id == id); true
+		return val.id == id;
+
+    });
+    */
+	//console.log('obj::::',obj); // { id: '15783879724635874', name: 'jly' }
+
+
+}	
+/*
+getAll()
+.then(data=>{
+	console.log(data);
+})
+.catch(err=>{
+	console.log(err);
+})
+*/
+
+
+/*
+async function update(id,name){
+	let arr = await readFile(filePath);
+
+	let strArr = JSON.parse(arr);;
+	let obj = strArr.find(val=>{
+		return val.id == id;
+	})
+
+	if(obj){
+		obj.name = name;
+		//console.log(obj);
+		//console.log(strArr)
+		let endStr = JSON.stringify(strArr);
+
+		await writeFile(filePath,endStr);
+		return strArr;
+
+	}else{
+		return obj;
+	}
+
+
+
+
+
+}
+update('15783901868604826','Amy')
+.then(data=>{
+	console.log('data:::',data);
+})
+.catch(err=>{
+	console.log(err);
+})
+*/
+
+
+async function remove(id){
+	let data = await readFile(filePath);
+	let arr = JSON.parse(data);
+
+	return newArr = arr.filter(val=>{
+		return val.id != id;
+	})
+	//console.log(newArr);
+
+
+
+}
+/*
+remove('15783879724635874')
+.then(data=>{
+	console.log(data);
+})
+.catch(err=>{
+	console.log('err:::',err);
+})
+*/
+
+
+module.exports = {
+    getAll,
+   	remove
+}
--- project(项目)/new wish/server.js
+++ project(项目)/new wish/server.js
@@ -4,13 +4,16 @@ const path = require('path');
 const port = 3000;
 const hostname = '127.0.0.1';
 const mime = require('./mime.json');
+const { getAll } = require('./module.js');
+//console.log(''+getAll);
 
 
 let server = http.createServer((req,res)=>{
-    console.log(req.url);
+    //console.log(req.url);
 
     if(req.url == '/' || req.url == '/index.html'){//获取首页
 
+        /*
         fs.readFile('./static/index.html',(err,data)=>{
             if(err){
                 res.setHeader('Content-Type','text/html;charset=utf-8');
@@ -21,15 +24,62 @@ let server = http.createServer((req,res)=>{
                 res.end(data);
             }
         });
+        */
+
+        getAll()
+        .then(data=>{
+            // console.log(data);
+           res.setHeader('Content-Type','text/html;charset=utf-8');
+       
+           let html = `<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                            <title>Document</title>
+                            <link rel="stylesheet" href="css/index.css">
+                            <link rel="stylesheet" href="css/reset.css">
+                        
+                        </head>
+                        <body>
+                            <div class="wall">`
+                            data.forEach(item=>{
+                                html+=`<div class="wish" style="background-color:${item.background};">
+                                    <a href="javascript:;" class="close" data-id='${item.id}'></a>
+                                    ${item.content}
+                                    </div>`
+
+                            })
+
+                           html+=` </div>
+                            <div class="form-box">
+                                <div>
+                                    <textarea name="content" id="content" cols="30" rows="20"></textarea>
+                                </div>
+                                <div>
+                                    <a href="javascript:;" class="sub-btn">许下心愿</a>
+                                </div>
+                            </div>
+                        </body>
+                        <script src="js/jquery-1.12.4.js"></script>
+                        <script src="js/jquery.pep.js"></script>
+                        <script src="js/index.js"></script>
+                        </html>` 
+           res.end(html);           
+        })
+
+    
 
     }else if(req.url == '/favicon.ico'){
         res.end('favicon');
     }else{
-        //获取static静态资源
+        //获取static静态资源css、js、img等
         let staticFilePath = path.normalize(__dirname + '/static/' + req.url);
 
-        let extname = path.extname(staticFilePath);
-        console.log(extname);
+        let extname = path.extname(staticFilePath);//获取静态资源文件格式
+        //console.log(extname);
+        
         fs.readFile(staticFilePath,(err,data)=>{
             if(err){
                 res.setHeader('Content-Type','text/html;charset=utf-8');
