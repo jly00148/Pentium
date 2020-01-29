1f8189e50327dafe9cbecda2e0ea5f3a7fbced2e
jly <453825818@qq.com>
2020-01-29 23:04:01 +0800

    blog-backend

--- project(项目)/blog-backend/app.js
+++ project(项目)/blog-backend/app.js
@@ -4,18 +4,24 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+
+swig.setDefaults({
+    cache:false
+})
+
 //配制应用模板
 app.engine('html',swig.renderFile); //html是模板名称，也是模板文件的扩展名，swig.renderFile是解析模板的方法
 
 //配制模板存放的目录
-app.set('views','/view');//第一个参数必须是views,第二个参数是文件存放的目录
+app.set('views','./views');//第一个参数必须是views,第二个参数是文件存放的目录
 
 //配制模板引擎
 app.set('view engine','html')//第一个参数必须是view engine，第二参数是模板名称
 
-app.get('/',(req,re)=>{
-    res.render('index');
+app.get('/',(req,res)=>{
+    res.render('main/index');
 })
+app.use(express.static('public'));
 
 app.listen(port,()=>{
     console.log(`server is connected at port ${port}`);
@@ -29,7 +35,7 @@ db.on('error',(err)=>{
     throw err;
 })
 
-db.on('open',(err,result)=>{
+db.once('open',(err,result)=>{
     if(err){
         console.log('open err',err);
     }else{
