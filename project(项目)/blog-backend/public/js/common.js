84f609e715aa46e15ba28dd0a36cc6e5e56d4660
jly <453825818@qq.com>
2020-01-31 23:51:51 +0800

    register

--- project(项目)/blog-backend/public/js/common.js
+++ project(项目)/blog-backend/public/js/common.js
@@ -3,6 +3,8 @@
     let $register = $('#register');
     let $goRegister = $('#go-register');
     let $goLogin = $('#go-login');
+    let $subRegister = $('#sub-register');
+    let $textDanger = $('.err');
 
     $goRegister.on('click',function(){
         $login.hide();
@@ -12,5 +14,36 @@
     $goLogin.on('click',function(){
         $login.show();
         $register.hide();
-    })    
+    })
+
+
+    $subRegister.on('click',function(){
+        var username = $register.find('[name="username"]').val();
+        var password = $register.find('[name="password"]').val();
+        var repassword = $register.find('[name="repassword"]').val();
+
+        //用户名字母开头，3-6位
+        
+        var errMsg = '';
+        if(!/^[a-z][0-9a-z]{3,6}$/i.test(username)){
+            errMsg = '以首字母开头，共4-7位';
+            // console.log(errMsg);
+            $textDanger.html(errMsg);
+        }
+        if(!/^\w{3,6}$/i.test(password)){
+            errMsg = '密码3-6位';
+            // console.log(errMsg);
+            $textDanger.html(errMsg);
+        }else if(repassword != password){
+            errMsg = '两次密码输入不一致';
+            console.log(errMsg);
+            return;
+        }else{
+            errMsg = '注册成功';
+            $textDanger.html(errMsg);
+            $.ajax({
+
+            })
+        }
+    })
 })(jQuery);
