1861a7815753f90f2aad2e1f249ecf75a3284a63
jly <453825818@qq.com>
2020-02-06 23:10:00 +0800

    react

--- new practice/react/03-todolist/src/index.js
+++ new practice/react/03-todolist/src/index.js
@@ -1,1 +1,40 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
 import './app.js';
+
+//写法一
+/*
+import {a,b} from './app.js';
+console.log(a,b);
+*/
+
+//写法二(同写法一)
+/*
+import {a,b} from './app.js';
+console.log(a,b);
+*/
+
+//写法三
+/*
+import {a as a1,b} from './app.js';
+console.log(a1,b);
+*/
+
+//写法四
+/*
+import {a as a1,b1} from './app.js';
+console.log(a1,b1);
+*/
+
+//写法五
+/*
+import b from "./app.js";;
+console.log(b);
+*/
+
+//写法六
+import b from "./app.js";;
+console.log(b);
+
+ReactDOM.render(<div><input/><button>按钮</button></div>,document.getElementById('root'));
