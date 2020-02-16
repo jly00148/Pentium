918896316f888a8a4de0105bca8098e1735e3eea
jly <453825818@qq.com>
2020-02-16 22:32:29 +0800

    react-redux

---
+++ new practice/react/07-todolist-react-redux/src/app.js
@@ -0,0 +1,34 @@
+import React,{ Component } from 'react';
+import {Button,Col,Row,Input,List} from 'antd';
+import './app.css';
+
+
+class App extends Component{
+    render(){
+        console.log('App render...');     
+        return(
+
+             <div className="App">
+             <Row>
+                <Col span={12}>
+                    <Input
+                    />
+                </Col>
+                <Col span={12}> 
+                    <Button type="primary">新增</Button>
+                </Col>
+             </Row>
+            
+                <List
+                    style = {{marginTop:10}}
+                    bordered
+                    dataSource = {[]}
+                    renderItem = {(item,index)=>(<List.Item>{item}</List.Item>)}
+                />
+            </div>
+        )
+
+    }
+}
+
+module.exports = App;
---
+++ new practice/react/07-todolist-react-redux/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');// 新用法
+// console.log(CleanWebpackPlugin);
+module.exports = {
+    mode:'development',
+    entry:{// 入口
+        index:'./src/index.js'
+    },
+    output:{
+        filename:'[name].[hash].bundle.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/i,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(jpg|jpeg|png|gif)$/i,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:10
+                        }
+                    }
+                ]
+            },
+
+            //babel
+            {
+                test:/\.js$/,
+                exclude:/(node_modules)/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['env','react'],
+                        plugins:[["import",{"libraryName":"antd","libraryDirectory":"es","style":"css"}]] //antd 按需索取css，不必加载全部css
+                    }
+                }
+            }
+        ]
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'index.html',
+            inject:true,
+            hash:true
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devServer:{
+        contentBase:'./dist',//内容的目录
+        port:8080//服务器运行的端口
+    }
+
+}
