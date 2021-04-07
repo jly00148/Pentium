debc48aa2ff9684d9a063e564e85e1be378d759f
jly <453825818@qq.com>
2021-04-07 15:34:34 +0800

    POST请求

--- 部分hcs练习/ajax/POST/server/server.js
+++ 部分hcs练习/ajax/POST/server/server.js
@@ -5,30 +5,41 @@ const url = require('url')
 var server = http.createServer((req,res)=>{
 	var urlStr = req.url;
 
-	if(urlStr.search(/\?/) != -1){//search必须有返回值
-		var parm = url.parse(urlStr,true).query 
-		res.setHeader('Content-Type','application/json')
-		res.end(JSON.stringify(parm))
-		return
+	if(req.method === 'GET'){
+		if(urlStr.search(/\?/) != -1){//search必须有返回值
+			var parm = url.parse(urlStr,true).query 
+			res.setHeader('Content-Type','application/json')
+			res.end(JSON.stringify(parm))
+			return
+		}
+
+		var filePath = '../'+ urlStr
+		var index = urlStr.indexOf('.');
+		var exte = urlStr.slice(index);
+
+		switch(exte){
+			case '.html': 
+				res.setHeader('Content-Type','text/html');
+			break;
+
+			case '.css': 
+				res.setHeader('Content-Type','text/css')
+			break;
+		}
+		var data = fs.readFileSync(filePath)
+		res.end(data)
+
+	}else if(req.method === 'POST'){
+		var body = '';
+		req.on('data',function(chunk){
+			body+=chunk;
+		})
+
+		req.on('end',function(){
+			res.setHeader('Content-Type','application/json')
+			res.end(body)
+		})
 	}
-
-	var filePath = '../'+ urlStr
-	var index = urlStr.indexOf('.');
-	var exte = urlStr.slice(index);
-
-	switch(exte){
-		case '.html': 
-			res.setHeader('Content-Type','text/html');
-		break;
-
-		case '.css': 
-			res.setHeader('Content-Type','text/css')
-		break;
-	}
-
-	var data = fs.readFileSync(filePath)
-	res.end(data)
-
 })
 
 
